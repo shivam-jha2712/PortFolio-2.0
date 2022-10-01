fcfa0895f0e96a1aa79d84e9f5c258b02de5c9a7
Shivam Jha <71783722+shivam-jha2712@users.noreply.github.com>
2022-10-01 13:02:20 +0530

    commit

--- backend_sanity/schemas/testimonials.js
+++ backend_sanity/schemas/testimonials.js
@@ -1,30 +1,30 @@
 export default {
-    name: 'testimonials',
-    title: 'Testimonials',
+    name:'testimonials',
+    title:'Testimonials',
     type: 'document',
-    fields: [
-        {
-            name: 'name',
-            title: 'Name',
-            type: 'string',
+    fields:[
+        { 
+            name:'name',
+            title:'Name',
+            type: 'string'
         },
         {
-            name: 'company',
-            title: 'Company',
-            type: 'string',
+            name:'company',
+            title:'Company',
+            type:'string'
         },
         {
-            name: 'imageurl',
-            title: 'ImgURL',
+            name:'imgurl',
+            title:'ImgUrl',
             type: 'image',
             options: {
-                hotspot: true,  // user could crop there photos accordingly.
-            }
-        },
+              hotspot: true,
+            },
+        },  
         {
-            name: 'feedback',
-            title: 'Feedback',
-            type: 'string',
-        },
-    ],
+            name:'feedback',
+            title:'Feedback',
+            type:'string'
+        }
+    ]
 }
---
+++ frontend_react/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <h1>App</h1>
+    );
+}
+
+export default App;
