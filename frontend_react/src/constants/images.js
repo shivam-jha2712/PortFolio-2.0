525b7838c93c9e82c7899a95e6a6ab11e10a359d
Shivam Jha <71783722+shivam-jha2712@users.noreply.github.com>
2022-12-03 16:20:56 +0530

    commit

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -19,10 +19,12 @@ import typescript from '../assets/typescript.png';
 import vue from '../assets/vue.png';
 import java from '../assets/java.png';
 
+import about011 from '../assets/about011.png';
 import about01 from '../assets/about01.png';
 import about02 from '../assets/about02.png';
 import about03 from '../assets/about03.png';
 import about04 from '../assets/about04.png';
+import about041 from '../assets/about041.png';
 
 import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
@@ -60,6 +62,8 @@ export default {
     about02,
     about03,
     about04,
+    about011,
+    about041,
     profile,
     circle,
     logo,
