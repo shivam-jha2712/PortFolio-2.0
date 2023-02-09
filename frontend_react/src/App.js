e75aa3d4e97db19deace9872cd1c6c58ddee69b7
Shivam Jha <71783722+shivam-jha2712@users.noreply.github.com>
2023-02-09 17:37:50 +0530

    commit

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,19 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 
 import { About, Education, Footer, Header, Skills, Testimonial, Work } from './container'
 import { Navbar } from './components';
 import './App.scss';
 const App = () => {
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 8000)
+    }, [])
+
     return (
         <div className="app">
-            <Navbar />
-            <Header />
-            <About />
-            <Education />
-            <Work />
-            <Skills />
-            <Testimonial />
-            <Footer />
+            {
+                loading ?
+                    <ClimbingBoxLoader color={"#000000"} loading={loading} size={30} />
+                    :
+                    <><Navbar /><Header /><About /><Education /><Work /><Skills /><Testimonial /><Footer /></>
+            }
         </div>
     );
 }
