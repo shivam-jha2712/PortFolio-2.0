6e7245bf6e0934867521d15b60da8abd7a0193e5
Shivam Jha <71783722+shivam-jha2712@users.noreply.github.com>
2023-06-13 16:35:22 +0530

    commit

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -16,7 +16,7 @@ const SocialMedia = () => {
                 </a>
             </div>
             <div>
-                <a href="https://drive.google.com/file/d/1h1qodYTKx0yZBVqk7IRMchWEqq0yFbUK/view?usp=share_link">
+                <a href="https://shorturl.at/hmAW1">
                     <FaFile />
                 </a>
             </div>
